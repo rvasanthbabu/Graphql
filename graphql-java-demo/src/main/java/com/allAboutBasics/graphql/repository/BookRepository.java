fc43c8c822277c37e2bbe8859657c1899e67d332
Vasanth Babu Raju <rvasanthbabu@gmail.com>
2021-10-31 18:16:05 +0530

    Delete graphql-java-demo directory

--- graphql-java-demo/src/main/java/com/allAboutBasics/graphql/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.allAboutBasics.graphql.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.allAboutBasics.graphql.model.Book;
-
-public interface BookRepository extends JpaRepository<Book, String> {
-}
